2a021759bc0e5c915d9ba3bbaaf76840e85756c6
Karonte <michael@sanelli.org>
2011-08-25 14:22:55 +0200

    Update

--- InformationTheory.py
+++ InformationTheory.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python
 from fractions import Fraction
 from decimal import Decimal
+from math import factorial
 import math
+
+def binomial(n, k):
+	if n>k:
+		return factorial(n) / (factorial(k)*factorial(n-k))
+	else:
+		if n==k:
+			return 1
+		else:
+			return 0
+
+def bernoulli_prob( n, k, p ):
+	return (p**k)*( (1-p) ** (n-k) )
 
 def entropy( c, pl):
 	x = 0
@@ -15,14 +28,13 @@ def num_seq_tip( c, n, pl ):
 	y = 2**(n*x)
 	return y
 
-def p_seq_tip( c, n, pl ):
+def prob_seq_tip( c, n, pl ):
 	x = entropy( c, pl )
 	y = 2** ( -1*(n*x))
 	return y
 
-pl = [0.5, 0.5]
-n = 1
-c = 1/math.log(2)
-print entropy( c, pl )
-print num_seq_tip( c, n, pl )
-print p_seq_tip(c, n, pl)
+def list_1of2powk(k):
+	x = []
+	for n in range (1,k+1):
+		x.append(Fraction(1,2**n))
+	return x;
